e9412d162157deb085ede412e1ef98b4591f6c34
Josh Pollock <josh@calderawp.com>
2018-03-22 16:56:55 -0400

    make fromRequest work for forms on service container

---
+++ Tests/Mocks/Provider.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace calderawp\interop\Mock;
+
+
+class Provider
+{
+
+}
--- src/Entities/Entry/Factory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace calderawp\interop\Entities\Entry;
-
-interface Factory extends \calderawp\interop\Entities\Factory
-{
-
-}
