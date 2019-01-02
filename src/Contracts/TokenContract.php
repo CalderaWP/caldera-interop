ba2fcf0ccfb59113fc48ea9f269727f6e94eb00c
Josh Pollock <josh@calderawp.com>
2019-01-01 19:24:35 -0500

    Csfr protection

---
+++ src/Contracts/TokenContract.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace calderawp\interop\Contracts;
+
+
+interface TokenContract
+{
+	/**
+	 * Generate a token, returning its public
+	 *
+	 * @return string
+	 */
+	public function getToken() : string;
+
+	/**
+	 * Test if string is a valid public for a token of current manager
+	 *
+	 * @param string $tokenStringToValidate
+	 *
+	 * @return bool
+	 */
+	public function validateToken(string $tokenStringToValidate):bool;
+}
