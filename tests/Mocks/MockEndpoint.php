5c518d3ce3dfe1cb63877e651216c1b589db4f4d
Josh Pollock <josh@calderawp.com>
2019-01-01 21:42:03 -0500

    render and submit contact form

--- tests/Mocks/MockEndpoint.php
+++ tests/Mocks/MockEndpoint.php
@@ -8,11 +8,19 @@ use calderawp\caldera\restApi\Exception;
 use calderawp\interop\Contracts\Rest\Endpoint;
 use calderawp\interop\Contracts\Rest\RestRequestContract as Request;
 use calderawp\interop\Contracts\Rest\RestResponseContract as Response;
+use calderawp\interop\Contracts\TokenContract;
 use calderawp\interop\Contracts\WordPress\ApplysFilters;
 use calderawp\interop\Traits\Rest\ProvidesRestEndpoint;
 
 class MockEndpoint extends MockCaldera implements Endpoint
 {
+	/**
+	 * @inheritDoc
+	 */
+	public function getToken(Request $request): string
+	{
+		return 'fds';
+	}
 
 	use ProvidesRestEndpoint;
 
