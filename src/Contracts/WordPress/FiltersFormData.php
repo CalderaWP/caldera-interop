5897d2802a7efc5cfed537968e83698e28744db9
Josh Pollock <josh@calderawp.com>
2019-01-06 12:34:09 -0500

    add acceptence tests to wordpress-package

---
+++ src/Contracts/WordPress/FiltersFormData.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace calderawp\interop\Contracts\WordPress;
+
+use \calderawp\caldera\Forms\Contracts\DataSourcesContract as FormsDataSources;
+use calderawp\interop\Contracts\WordPress\ApplysFilters;
+use calderawp\caldera\DataSource\Contracts\SourceContract as Source;
+
+interface FiltersFormData
+{
+
+}
